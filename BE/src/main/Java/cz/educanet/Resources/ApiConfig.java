9e453a15353b51a90b59ec7a39faa80d38640281
j-forgac <62855827+j-forgac@users.noreply.github.com>
2021-03-05 12:01:15 +0100

    a

--- BE/src/main/Java/cz/educanet/Resources/ApiConfig.java
+++
@@ -1,9 +0,0 @@
-package cz.educanet.Resources;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("api")
-public class ApiConfig extends Application {
-
-}
