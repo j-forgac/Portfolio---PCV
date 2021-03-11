f53349b4c8dafdaf4ea03cd07da351c57f48d6ca
j-forgac <62855827+j-forgac@users.noreply.github.com>
2021-03-11 22:27:45 +0100

    novy design + castecna editace

--- FE/src/app/user/user.component.ts
+++ FE/src/app/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {UserData} from './UserData';
 
 @Component({
   selector: 'app-user',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  myMap: Map<number, string> = new Map();
+  text = '';
+  isEditing = false;
+  userData: UserData[] = [];
+
+  constructor() {
+    /*this.myMap = new Map();*/
+
+  }
 
   ngOnInit(): void {
+  }
+
+  addUserData(): void{
+    this.isEditing = !this.isEditing;
+    this.userData.push(new UserData(this.text));
+    console.log(this.text);
+    console.log(this.userData);
   }
 
 }
