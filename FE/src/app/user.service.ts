1bf5e5e69d4538282080bc4fc28ed61d72a90ac8
KosacekCZ <35407839+KosacekCZ@users.noreply.github.com>
2021-03-19 13:53:45 +0100

    Git + FE design

--- FE/src/app/user.service.ts
+++ FE/src/app/user.service.ts
@@ -9,7 +9,7 @@ export class UserService {
 
   constructor(private httpClient: HttpClient) {
   }
-  HttpPostRegistrace(username: string, heslo: string, name: string, popis: string, skills: string): Observable<any> {
+  registrace(username: string, heslo: string, name: string, popis: string, skills: string): Observable<any> {
     return this.httpClient.post('http://127.0.0.1:8080/BE/api/users/create', {
       username,
       heslo,
