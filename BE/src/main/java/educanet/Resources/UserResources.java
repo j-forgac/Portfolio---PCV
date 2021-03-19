5af40b3e56fd807e111e6e66c21da9d913118c83
skyper-cz <62872636+skyper-cz@users.noreply.github.com>
2021-03-19 10:49:49 +0100

    BE funguje

--- BE/src/main/java/educanet/Resources/UserResources.java
+++ BE/src/main/java/educanet/Resources/UserResources.java
@@ -7,7 +7,7 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("Users")
+@Path("users")
 @Produces(MediaType.APPLICATION_JSON)
 
 public class UserResources {
